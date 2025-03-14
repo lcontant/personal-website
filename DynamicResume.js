76e6841c65582e84f2a4cf298675277169578656
Louis Contant <louis.contant@radio-canada.ca>
2025-03-14 08:58:59 -0400

    Start mapping dynamic data

--- DynamicResume.js
+++ DynamicResume.js
@@ -1,47 +1,29 @@
-let currentLanguage = document.location.pathname.toLowerCase().includes('en') ? 'en' : 'fr';
-let resumeData = {};
-let jobs = [];
-let personalProjects = [];
-document.addEventListener('DOMContentLoaded', () => {
+var resumeData = {};
+async function getResumeData() {
     let resumeDataFileName = `CV_${currentLanguage}_standard.json`;
-    fetch(resumeDataFileName)
-        .then(response => response.json())
-        .then(data => {
-            resumeData = data;
-            let jobId = 0;
-            let taskId = 0;
-            let techStackId = 0;
-            for (let job of resumeData.experience) {
-                job.id = jobId++;
-                let techStack = [];
-                let accomplishments = [];
-                for (let task of job.tasks) {
-                    task.id = taskId++;
-                    accomplishments.push({
-                        id: taskId,
-                        description: task
-                    });
-                }
-                for (let techStack of job.techStack) {
-                    techStack.id = techStackId++;
-                    techStack.push({
-                        id: techStackId,
-                        tech: techStack
-                    });
-                }
-                let jobData = {
-                    id: job.id,
-                    title: job.role,
-                    logo: job.logo,
-                    company: job.company,
-                    duration: job.duration,
-                    accomplishments: accomplishments,
-                    techStack: techStack
-                };
-                jobs.push(jobData);
-            }
-        })
-        .catch(error => console.error('Error loading JSON:', error));
-});
-function setAccomplishement(jobId, accomplishment, index) {
+    let response = await fetch(resumeDataFileName);
+    let data = response.json();
+    resumeData = data;
+    let jobId = 0;
+    for (let jsonJob of resumeData.work) {
+        let job = {
+            id: jobId++,
+            job: jsonJob
+        };
+        jobs.push(job);
+    }
 }
+
+async function renderContactInfo() {
+    let address = resumeData.basics.location.CountryCode + ',' + resumeData.basics.location.region + ',' + resumeData.location.City;
+}
+
+function renderJob() {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getResumeData();
+    renderContactInfo();
+    renderJob();
+});
